f5aab45cb3dc5473c1119d3981452b47631eeda3
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-27 17:12:34 -0500

    Reader working

--- srv_persistor/main_profe.py
+++ srv_persistor/main_profe.py
@@ -9,13 +9,19 @@ HOST = '[::]:50051'
 
 def serve():
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        monedas_pb2_grpc.add_MonedasServicer_to_server(monedas_pb2_grpc.Monedas(), server)
+        monedas_pb2_grpc.add_MonedasServicer_to_server(Monedas(), server)
         server.add_insecure_port(HOST)
         print("Service persistor is running... ")
         server.start()
         server.wait_for_termination()
 
-#class Monedas(monedas_pb2_grpc.MonedasServicer):
+#Link el microservicio con una clase de python
+class Monedas(monedas_pb2_grpc.MonedasServicer):
+      
+      def PingMonedas(self, request, context):
+            #creamos un objeto respuesta
+            response = monedas_pb2.PingMonedasResponse(ack='1')
+            return response
    
 #    def AddProduct(self, request, context):
 #       print("Request is received: " + str(request))
--- srv_reader/main.py
+++ srv_reader/main.py
@@ -2,6 +2,7 @@ import grpc
 import monedas_pb2
 import monedas_pb2_grpc
 
+HOST = '[srv_persistor]:50051'
 
 def serve():
 	#ESTE ES EL CONSUMER
@@ -9,7 +10,7 @@ def serve():
 	with grpc.insecure_channel('srv_persistor:50051') as channel:
 		stub = monedas_pb2_grpc.MonedasStub(channel)
 		request = monedas_pb2.EmptyMessage()
-		response= stub.PingMonedas(request)
+		response = stub.PingMonedas(request)
 		print("Recived: ", response.ack)
 
 
