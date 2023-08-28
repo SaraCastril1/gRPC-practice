1d1bd1a065b4e4a13d62ad38b64adc72efa18b46
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-27 19:55:17 -0500

    Reading data from a csv

--- srv_reader/main.py
+++ srv_reader/main.py
@@ -2,16 +2,22 @@ import grpc
 import monedas_pb2
 import monedas_pb2_grpc
 
+from repositories.File_repository import File_repository
+
 HOST = '[srv_persistor]:50051'
 
 def serve():
-	#ESTE ES EL CONSUMER
-	#Aqui se debe levantar la conexion
-	with grpc.insecure_channel('srv_persistor:50051') as channel:
-		stub = monedas_pb2_grpc.MonedasStub(channel)
-		request = monedas_pb2.EmptyMessage()
-		response = stub.PingMonedas(request)
-		print("Recived: ", response.ack)
+	#Codigo estra -------------------------------------------------
+	file_repository = File_repository('tmp/data/BTCUSD_M5.csv')
+	readed_data = file_repository.__read_data()
+	#--------------------------------------------------------------
+	for row in readed_data:
+		#Aqui se debe levantar la conexion
+		with grpc.insecure_channel('srv_persistor:50051') as channel:
+			stub = monedas_pb2_grpc.MonedasStub(channel)
+			request = monedas_pb2.EmptyMessage()
+			response = stub.PingMonedas(request)
+			print("Recived: ", response.ack)
 
 
 if __name__ == "__main__":
